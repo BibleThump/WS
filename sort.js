8718042bc0ed65294cc02a7509a572f9d3be18ac
BibleThump <challeg@hotmail.se>
2016-01-27 09:32:34 +0100

    java script funkar nästan och fortsatter på sidan

--- sort.js
+++ sort.js
@@ -1,11 +1,19 @@
 function changeweapon() {
 	document.getElementsByClassName("weapon")[0].style.display="inline";
-	document.getElementsByClassName("minion spell")[0].style.display="none";
+	document.getElementsByClassName("minion")[0].style.display="none";
+	document.getElementsByClassName("spell")[0].style.display="none";
+}
 function changeminion() {
-	document.getElementsByClassName("weapon spell")[0].display="none";
-	document.getElementsByClassName("minion")[0].display="inline";
+	document.getElementsByClassName("weapon")[0].style.display="none";
+	document.getElementsByClassName("minion")[0].style.display="inline";
+	document.getElementsByClassName("spell")[0].style.display="none";
 }
 function changespell() {
-	document.getElementsByClassName("minion weapon")[0].style.display="none";
+	document.getElementsByClassName("weapon")[0].style.display="none";
+	document.getElementsByClassName("minion")[0].style.display="none";
 	document.getElementsByClassName("spell")[0].style.display="inline";
 }
+function changeall() {
+	document.getElementsByClassName("weapon")[0].style.display="inline";
+	document.getElementsByClassName("minion")[0].style.display="inline";
+	document.getElementsByClassName("spell")[0].style.display="inline";
