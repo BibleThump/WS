5b5253fce482650926b43a31b2a84e1ccd87c9ec
BibleThump <challeg@hotmail.se>
2016-01-27 08:42:53 +0100

    startar kort sidan

--- sort.js
+++ sort.js
@@ -1,11 +1,11 @@
 function changeweapon() {
-	document.getElementsByClassName("weapon").style.display="inline";
-	document.getElementsByClassName("minion spell").style.display="none";
+	document.getElementsByClassName("weapon")[0].style.display="inline";
+	document.getElementsByClassName("minion spell")[0].style.display="none";
 function changeminion() {
-	document.getElementsByClassName("weapon spell").display="none";
-	document.getElementsByClassName("minion").display="inline";
+	document.getElementsByClassName("weapon spell")[0].display="none";
+	document.getElementsByClassName("minion")[0].display="inline";
 }
 function changespell() {
-	document.getElementsByClassName("minion weapon").style.display="none";
-	document.getElementsByClassName("spell").style.display="inline";
+	document.getElementsByClassName("minion weapon")[0].style.display="none";
+	document.getElementsByClassName("spell")[0].style.display="inline";
 }
