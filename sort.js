5c8cf5b92f0f7bd854e61d5b520e761b2a732e76
BibleThump <challeg@hotmail.se>
2016-02-03 18:57:17 +0100

    ändrade lite på bilderna

--- sort.js
+++ sort.js
@@ -1,8 +1,9 @@
-function BeastialWrathCV(){
+<!-- hunter -->
+function BeastialWrathBV(){
 	document.getElementById("BeastialWrathB").src="";
- 	document.getElementById("BeastialWrathV").src="http://media-hearth.cursecdn.com/goldCards/0/638/638.webm";
+ 	document.getElementById("BeastialWrathV").src="http://media-hearth.cursecdn.com/goldCards/0/304/304.webm";
 }
-function BeastialWrathCB(){
+function BeastialWrathBB(){
 	document.getElementById("BeastialWrathB").src="http://media-hearth.cursecdn.com/avatars/148/421/304.png";
  	document.getElementById("BeastialWrathV").src="";
 }
