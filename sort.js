e195e2ac605407522c1ead9fa8ce5ce4818c09c2
BibleThump <challeg@hotmail.se>
2016-02-03 21:20:44 +0100

    mer JavaScript

--- sort.js
+++ sort.js
@@ -7,3 +7,295 @@ function BeastialWrathBB(){
 	document.getElementById("BeastialWrathB").src="http://media-hearth.cursecdn.com/avatars/148/421/304.png";
  	document.getElementById("BeastialWrathV").src="";
 }
+
+
+function BraveArcherBV(){
+	document.getElementById("BraveArcherB").src="";
+ 	document.getElementById("BraveArcherV").src="länk video";
+}
+function BraveArcherBB(){
+	document.getElementById("BRaveArcherB").src="länk bild";
+ 	document.getElementById("BraveArcherV").src="";
+}
+
+
+function GlaivezookaBV(){
+	document.getElementById("GlaivezookaB").src="";
+ 	document.getElementById("GlaivezookaV").src="länk video";
+}
+function GlaivezookaBB(){
+	document.getElementById("GlaivezookaB").src="länk bild";
+ 	document.getElementById("GlaivezookaV").src="";
+}
+
+
+function BearTrapBV(){
+	document.getElementById("BearTrapB").src="";
+ 	document.getElementById("BearTrapV").src="länk video";
+}
+function BearTrapBB(){
+	document.getElementById("BearTrapB").src="länk bild";
+ 	document.getElementById("BearTrapV").src="";
+}
+
+
+function CallPetBV(){
+	document.getElementById("CallpetB").src="";
+ 	document.getElementById("CallpetV").src="länk video";
+}
+function CallPetBB(){
+	document.getElementById("CallpetB").src="länk bild";
+ 	document.getElementById("CallPetV").src="";
+}
+
+
+function ExplosiveTrapBV(){
+	document.getElementById("ExplosiveTrapB").src="";
+ 	document.getElementById("ExplosiveTrapV").src="länk video";
+}
+function ExplosiveTrapBB(){
+	document.getElementById("ExplosiveTrapB").src="länk bild";
+ 	document.getElementById("ExplosiveTrapV").src="";
+}
+
+
+function FeignDeathBV(){
+	document.getElementById("FeignDeathB").src="";
+ 	document.getElementById("FeignDeathV").src="länk video";
+}
+function FeignDeathBB(){
+	document.getElementById("FeignDeathB").src="länk bild";
+ 	document.getElementById("FeignDeathV").src="";
+}
+
+
+function FlareBV(){
+	document.getElementById("FlareB").src="";
+ 	document.getElementById("FlareV").src="länk video";
+}
+function FlareBB(){
+	document.getElementById("FlareB").src="länk bild";
+ 	document.getElementById("FlareV").src="";
+}
+
+
+function FreezingTrapBV(){
+	document.getElementById("FreezingTrapB").src="";
+ 	document.getElementById("FreezingTrapV").src="länk video";
+}
+function FreezingTrapBB(){
+	document.getElementById("FreezingTrapB").src="länk bild";
+ 	document.getElementById("FreezingTrapV").src="";
+}
+
+
+function LockAndLoadBV(){
+	document.getElementById("LockAndLoadB").src="";
+ 	document.getElementById("LockAndLoadV").src="länk video";
+}
+function LockAndLoadBB(){
+	document.getElementById("LockAndLoadB").src="länk bild";
+ 	document.getElementById("LockAndLoadV").src="";
+}
+
+
+function MisdirectionBV(){
+	document.getElementById("MisdirectionB").src="";
+ 	document.getElementById("MisdirectionV").src="länk video";
+}
+function MisdirectionBB(){
+	document.getElementById("MisdirectionB").src="länk bild";
+ 	document.getElementById("MisdirectionV").src="";
+}
+
+
+function SnakeTrapBV(){
+	document.getElementById("SnakeTrapB").src="";
+ 	document.getElementById("SnakeTrapV").src="länk video";
+}
+function SnakeTrapBB(){
+	document.getElementById("SnakeTrapB").src="länk bild";
+ 	document.getElementById("SnakeTrapV").src="";
+}
+
+
+function SnipeBV(){
+	document.getElementById("SnipeB").src="";
+ 	document.getElementById("SnipeV").src="länk video";
+}
+function SnipeBB(){
+	document.getElementById("SnipeB").src="länk bild";
+ 	document.getElementById("SnipeV").src="";
+}
+
+
+function KingsElekkBV(){
+	document.getElementById("KingsElekkB").src="";
+ 	document.getElementById("KingsElekkV").src="länk video";
+}
+function KingsElekkBB(){
+	document.getElementById("KingsElekkB").src="länk bild";
+ 	document.getElementById("KingsElekkV").src="";
+}
+
+
+function ScavengingHyenaBV(){
+	document.getElementById("ScavengingHyenaB").src="";
+ 	document.getElementById("ScavengingHyenaV").src="länk video";
+}
+function ScavengingHyenaBB(){
+	document.getElementById("ScavengingHyenaB").src="länk bild";
+ 	document.getElementById("ScavengingHyenaV").src="";
+}
+
+
+function SteamwheedleSniperBV(){
+	document.getElementById("SteamwheedleSniperB").src="";
+ 	document.getElementById("SteamwheedleSniperV").src="länk video";
+}
+function SteamwheedleSniperBB(){
+	document.getElementById("SteamwheedleSniperB").src="länk bild";
+ 	document.getElementById("SteamwheedleSniperV").src="";
+}
+
+
+function EaglehornBowBV(){
+	document.getElementById("EaglehornBowB").src="";
+ 	document.getElementById("EaglehornBowV").src="länk video";
+}
+function EaglehornBowBB(){
+	document.getElementById("EaglehornBowB").src="länk bild";
+ 	document.getElementById("EaglehornBowV").src="";
+}
+
+
+function DeadlyShotBV(){
+	document.getElementById("DeadlyShotB").src="";
+ 	document.getElementById("DeadlyShotV").src="länk video";
+}
+function DeadlyShotBB(){
+	document.getElementById("DeadlyShotB").src="länk bild";
+ 	document.getElementById("DeadlyShotV").src="";
+}
+
+
+function PowerShotBV(){
+	document.getElementById("PowerShotB").src="";
+ 	document.getElementById("PowerShotV").src="länk video";
+}
+function PowerShotBB(){
+	document.getElementById("PowerShotB").src="länk bild";
+ 	document.getElementById("PowerShotV").src="";
+}
+
+
+function UnleashTheHoundsBV(){
+	document.getElementById("UnleashTheHoundsB").src="";
+ 	document.getElementById("UnleashTheHoundsV").src="länk video";
+}
+function UnleashTheHoundsBB(){
+	document.getElementById("UnleashTheHoundsB").src="länk bild";
+ 	document.getElementById("UnleashTheHoundsV").src="";
+}
+
+
+function DreadscaleBV(){
+	document.getElementById("DreadscaleB").src="";
+ 	document.getElementById("DreadscaleV").src="länk video";
+}
+function DreadscaleBB(){
+	document.getElementById("DreadscaleB").src="länk bild";
+ 	document.getElementById("DreadscaleV").src="";
+}
+
+
+function MetaltoothLeaperBV(){
+	document.getElementById("MetaltoothLeaperB").src="";
+ 	document.getElementById("MetaltoothLeaperV").src="länk video";
+}
+function MetaltoothLeaperBB(){
+	document.getElementById("MetaltoothLeaperB").src="länk bild";
+ 	document.getElementById("MetaltoothLeaperV").src="";
+}
+
+
+function StablemasterBV(){
+	document.getElementById("StablemasterB").src="";
+ 	document.getElementById("StablemasterV").src="länk video";
+}
+function StablemasterBB(){
+	document.getElementById("StablemasterB").src="länk bild";
+ 	document.getElementById("StablemasterV").src="";
+}
+
+
+function CobraShotBV(){
+	document.getElementById("CobraShotB").src="";
+ 	document.getElementById("CobraShotV").src="länk video";
+}
+function CobraShotBB(){
+	document.getElementById("CobraShotB").src="länk bild";
+ 	document.getElementById("CobraShotV").src="";
+}
+
+
+function ExplosiveShotBV(){
+	document.getElementById("ExplosiveShotB").src="";
+ 	document.getElementById("ExplosiveShotV").src="länk video";
+}
+function ExplosiveShotBB(){
+	document.getElementById("ExplosiveShotB").src="länk bild";
+ 	document.getElementById("ExplosiveShotV").src="";
+}
+
+
+function KingOfBeastsBV(){
+	document.getElementById("KingOfBeastsB").src="";
+ 	document.getElementById("KingOfBeastsV").src="länk video";
+}
+function KingOfBeastsBB(){
+	document.getElementById("KingOfBeastsB").src="länk bild";
+ 	document.getElementById("KingOfBeastsV").src="";
+}
+
+
+function RamWranglerBV(){
+	document.getElementById("RamWranglerB").src="";
+ 	document.getElementById("RamWranglerV").src="länk video";
+}
+function RamWranglerBB(){
+	document.getElementById("RamWranglerB").src="länk bild";
+ 	document.getElementById("RamWranglerV").src="";
+}
+
+
+function BallOfSpidersBV(){
+	document.getElementById("BallOfSpidersB").src="";
+ 	document.getElementById("BallOfSpidersV").src="länk video";
+}
+function BallOfSpidersBB(){
+	document.getElementById("BallOfSpidersB").src="länk bild";
+ 	document.getElementById("BallOfSpidersV").src="";
+}
+
+
+function SavannahHighmaneBV(){
+	document.getElementById("SavannahHighmaneB").src="";
+ 	document.getElementById("SavannahHighmaneV").src="länk video";
+}
+function SavannahHighmaneBB(){
+	document.getElementById("SavannahHighmaneB").src="länk bild";
+ 	document.getElementById("SavannahHighmaneV").src="";
+}
+
+
+function (){
+	document.getElementById("").src="";
+ 	document.getElementById("").src="länk video";
+}
+function (){
+	document.getElementById("").src="länk bild";
+ 	document.getElementById("").src="";
+}
+
+
