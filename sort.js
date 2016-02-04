3018d5a5a2f7bf5d36ef37875a6aac376c13e8aa
BibleThump <challeg@hotmail.se>
2016-02-04 22:43:35 +0100

    uppdaterade javascript

--- sort.js
+++ sort.js
@@ -1,4 +1,108 @@
 //hunter
+var guld = 0;
+function ChangeH(){
+	guld +=1;
+	if(guld==1){
+	document.getElementById("BeastialWrathB").src="";
+ 	document.getElementById("BeastialWrathV").src="http://media-hearth.cursecdn.com/goldCards/0/304/304.webm";
+
+ 	document.getElementById("BraveArcherB").src="";
+ 	document.getElementById("BraveArcherV").src="http://media-hearth.cursecdn.com/goldCards/22/310/22310.webm";
+
+ 	document.getElementById("GlaivezookaB").src="";
+ 	document.getElementById("GlaivezookaV").src="http://media-hearth.cursecdn.com/goldCards/12/267/12267.webm";
+
+ 	document.getElementById("BearTrapB").src="";
+ 	document.getElementById("BearTrapV").src="http://media-hearth.cursecdn.com/goldCards/22/364/22364.webm";
+
+	document.getElementById("CallpetB").src="";
+ 	document.getElementById("CallpetV").src="http://media-hearth.cursecdn.com/goldCards/12/224/12224.webm";
+
+	document.getElementById("ExplosiveTrapB").src="";
+ 	document.getElementById("ExplosiveTrapV").src="http://media-hearth.cursecdn.com/goldCards/0/344/344.webm";
+
+	document.getElementById("FeignDeathB").src="";
+ 	document.getElementById("FeignDeathV").src="http://media-hearth.cursecdn.com/goldCards/12/238/12238.webm";
+
+	document.getElementById("FlareB").src="";
+ 	document.getElementById("FlareV").src="http://media-hearth.cursecdn.com/goldCards/0/630/630.webm";
+
+	document.getElementById("FreezingTrapB").src="";
+ 	document.getElementById("FreezingTrapV").src="http://media-hearth.cursecdn.com/goldCards/0/99/99.webm";
+
+	document.getElementById("LockAndLoadB").src="";
+ 	document.getElementById("LockAndLoadV").src="http://media-hearth.cursecdn.com/goldCards/22/258/22258.webm";
+
+	document.getElementById("MisdirectionB").src="";
+ 	document.getElementById("MisdirectionV").src="http://media-hearth.cursecdn.com/goldCards/0/447/447.webm";
+
+	document.getElementById("SnakeTrapB").src="";
+ 	document.getElementById("SnakeTrapV").src="http://media-hearth.cursecdn.com/goldCards/0/210/210.webm";
+
+	document.getElementById("SnipeB").src="";
+ 	document.getElementById("SnipeV").src="http://media-hearth.cursecdn.com/goldCards/0/553/553.webm";
+
+	document.getElementById("KingsElekkB").src="";
+ 	document.getElementById("KingsElekkV").src="http://media-hearth.cursecdn.com/goldCards/22/370/22370.webm";
+
+	document.getElementById("ScavengingHyenaB").src="";
+ 	document.getElementById("ScavengingHyenaV").src="http://media-hearth.cursecdn.com/goldCards/0/279/279.webm";
+
+ 	document.getElementById("SteamwheedleSniperB").src="";
+ 	document.getElementById("SteamwheedleSniperV").src="http://media-hearth.cursecdn.com/goldCards/12/242/12242.webm";
+
+	document.getElementById("EaglehornBowB").src="";
+ 	document.getElementById("EaglehornBowV").src="http://media-hearth.cursecdn.com/goldCards/0/363/363.webm";
+
+	document.getElementById("DeadlyShotB").src="";
+ 	document.getElementById("DeadlyShotV").src="http://media-hearth.cursecdn.com/goldCards/0/239/239.webm";
+
+	document.getElementById("PowerShotB").src="";
+ 	document.getElementById("PowerShotV").src="http://media-hearth.cursecdn.com/goldCards/22/384/22384.webm";
+
+	document.getElementById("UnleashTheHoundsB").src="";
+ 	document.getElementById("UnleashTheHoundsV").src="http://media-hearth.cursecdn.com/goldCards/0/317/317.webm";
+
+	document.getElementById("DreadscaleB").src="";
+ 	document.getElementById("DreadscaleV").src="http://media-hearth.cursecdn.com/goldCards/22/389/22389.webm";
+
+	document.getElementById("MetaltoothLeaperB").src="";
+ 	document.getElementById("MetaltoothLeaperV").src="http://media-hearth.cursecdn.com/goldCards/12/254/12254.webm";
+
+	document.getElementById("StablemasterB").src="";
+ 	document.getElementById("StablemasterV").src="http://media-hearth.cursecdn.com/goldCards/22/365/22365.webm";
+
+	document.getElementById("CobraShotB").src="";
+ 	document.getElementById("CobraShotV").src="http://media-hearth.cursecdn.com/goldCards/12/304/12304.webm";
+
+	document.getElementById("ExplosiveShotB").src="";
+ 	document.getElementById("ExplosiveShotV").src="http://media-hearth.cursecdn.com/goldCards/0/114/114.webm";
+
+	document.getElementById("KingOfBeastsB").src="";
+ 	document.getElementById("KingOfBeastsV").src="http://media-hearth.cursecdn.com/goldCards/12/285/12285.webm";
+
+	document.getElementById("RamWranglerB").src="";
+ 	document.getElementById("RamWranglerV").src="http://media-hearth.cursecdn.com/goldCards/22/344/22344.webm";
+
+	document.getElementById("BallOfSpidersB").src="";
+ 	document.getElementById("BallOfSpidersV").src="http://media-hearth.cursecdn.com/goldCards/22/272/22272.webm";
+
+	document.getElementById("SavannahHighmaneB").src="";
+ 	document.getElementById("SavannahHighmaneV").src="http://media-hearth.cursecdn.com/goldCards/0/8/8.webm";
+
+	document.getElementById("GladiatorsLongbowB").src="";
+ 	document.getElementById("GladiatorsLongbowV").src="http://media-hearth.cursecdn.com/goldCards/0/278/278.webm";
+
+	document.getElementById("AcidmawB").src="";
+	document.getElementById("AcidmawV").src="http://media-hearth.cursecdn.com/goldCards/22/393/22393.webm";
+
+	document.getElementById("GahzrillaB").src="";
+ 	document.getElementById("GahzrillaV").src="http://media-hearth.cursecdn.com/goldCards/12/232/12232.webm";
+
+	document.getElementById("KingKrushB").src="";
+ 	document.getElementById("KingKrushV").src="http://media-hearth.cursecdn.com/avatars/148/415/194.png";
+}
+}
 
 function BeastialWrathBV(){
 	document.getElementById("BeastialWrathB").src="";
@@ -328,6 +432,14 @@ function KingKrushBB(){
 	document.getElementById("KingKrushB").src="http://media-hearth.cursecdn.com/avatars/148/415/194.png";
  	document.getElementById("KingKrushV").src="";
 }
+
+
+
+
+
+
+
+
 
 
 
